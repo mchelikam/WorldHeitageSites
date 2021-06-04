a2852f5d5ac3dbb1e89572fd0d887fe21fa0b28d
mchelikam <77543415+mchelikam@users.noreply.github.com>
2021-06-04 11:24:14 -0400

    Update

--- WorldHeritageJSON/WorldHeritageJSON/Pages/WorldHeritageSites.cshtml.cs
+++ WorldHeritageJSON/WorldHeritageJSON/Pages/WorldHeritageSites.cshtml.cs
@@ -9,16 +9,19 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using QuickType;
 using QuickTypeWeather;
+using SunriseSunset;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Schema;
+using Microsoft.Extensions.Configuration;
+
 
 
 namespace WorldHeritageJSON.Pages
 {
-    
+
     public class Item
     {
-       
+
         public double lat { get; set; }
         public double lon { get; set; }
 
@@ -28,47 +31,96 @@ namespace WorldHeritageJSON.Pages
 
         public double tem { get; set; }
 
+        public string sr { get; set; }
+        public string ss { get; set; }
+
     }
     public class WorldHeritageSitesModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public WorldHeritageSitesModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [BindProperty]
+        public string UnescoCountry { get; set; }
+
+        public bool IsSearchCountry { get; set; }
+        public bool IsSearchValid = true;
+
         
+        public bool IsSearchEmpty = false;
+
         public void OnGet()
         {
-    
-            using (var webClient = new WebClient())
+            IsSearchCountry = false;
+        }
+        public void OnPost()
+        {
+            try
             {
-
-                string key=System.IO.File.ReadAllText("WeatherAPIKey.txt");
-                
-
-                String jsonString = webClient.DownloadString("https://examples.opendatasoft.com/api/records/1.0/search/?dataset=world-heritage-unesco-list&q=&rows=1052&facet=category&facet=country_en&facet=country_fr&facet=continent_en&facet=continent_fr");
-                JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("unesco.json"));
-                JObject jsonObject = JObject.Parse(jsonString);
-                QuickType.Welcome welcome = QuickType.Welcome.FromJson(jsonString);
-                List<Item> items = new List<Item>();
-                double t;
-                for (int i = 0; i < 10; i++)
+                if (string.IsNullOrEmpty(UnescoCountry))
                 {
-                    Fields field = welcome.Records[i].Fields;
-                    
-                    {
-                        string weatherString = webClient.DownloadString("https://api.weatherbit.io/v2.0/current?lat=" + field.Latitude + "&lon=" + field.Longitude + "&key=" + key + "&include=minutely");
-                        //JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("unesco.json"));
-                        //JObject jsonObject = JObject.Parse(jsonString);
-                        QuickTypeWeather.Welcome welcomeWeather = QuickTypeWeather.Welcome.FromJson(weatherString);
-                        
-                        t = welcomeWeather.Data[0].Temp;
-                        //new Item{ lat=field.Latitude, lon= field.Longitude }
-                        items.Add(new Item { lat = field.Latitude, lon = field.Longitude, country = field.CountryEn, name = field.NameEn, tem = t });
-
-                };
-                   
-                    ViewData["Items"] = items;
-                   
-                    
+                    IsSearchEmpty = true;
                 }
-                
-                
+                else
+                {
+                    string CountrySearched = UnescoCountry.ToLower();
+
+                    using (var webClient = new WebClient())
+                    {
+
+                        string key = System.IO.File.ReadAllText("WeatherAPIKey.txt");
+
+
+                        String jsonString = webClient.DownloadString("https://examples.opendatasoft.com/api/records/1.0/search/?dataset=world-heritage-unesco-list&q=&rows=1052&facet=category&facet=country_en&facet=country_fr&facet=continent_en&facet=continent_fr");
+                        JSchema schema = JSchema.Parse(System.IO.File.ReadAllText("unesco.json"));
+                        JObject jsonObject = JObject.Parse(jsonString);
+                        QuickType.Welcome welcome = QuickType.Welcome.FromJson(jsonString);
+                        List<Item> items = new List<Item>();
+                        double t;
+                        string sunrise;
+                        string sunset;
+                        for (int i = 0; i < 1052; i++)
+                        {
+                            Fields field = welcome.Records[i].Fields;
+                            if (CountrySearched == field.CountryEn.ToLower())
+                            {
+
+                                {
+                                    string weatherString = webClient.DownloadString("https://api.weatherbit.io/v2.0/current?lat=" + field.Latitude + "&lon=" + field.Longitude + "&key=" + key + "&include=minutely");
+
+                                    QuickTypeWeather.Welcome welcomeWeather = QuickTypeWeather.Welcome.FromJson(weatherString);
+
+                                    string SunriseSunsetString = webClient.DownloadString("https://api.sunrise-sunset.org/json?lat=" + field.Latitude + "&lng=" + field.Longitude);
+                                    SunriseSunset.HighFive RiseSet = SunriseSunset.HighFive.FromJson(SunriseSunsetString);
+                                    sunrise = RiseSet.Results.Sunrise;
+                                    sunset = RiseSet.Results.Sunset;
+                                    var roundedLat = Math.Round(field.Latitude, 5);
+                                    var roundedLong = Math.Round(field.Longitude, 5);
+
+                                    t = welcomeWeather.Data[0].Temp;
+
+                                    items.Add(new Item { lat = roundedLat, lon = roundedLong, country = field.CountryEn, name = field.NameEn, tem=t, sr=sunrise, ss=sunset});
+
+                                };
+                                if (items.Count != 0)
+                                    ViewData["Items"] = items;
+                                else
+                                    ViewData["IsSearchValid"] = false;
+                            }
+
+
+                        }
+                    }
+
+                    IsSearchCountry = true;
+
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception occured while searching the UNESCO sites");
             }
         }
     }
